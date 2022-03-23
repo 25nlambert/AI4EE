e0dfa1e0fd62351357e6ef2d5a731d68a6daf337
25nlambert <82062702+25nlambert@users.noreply.github.com>
2022-03-23 12:15:57 -0400

    Add files via upload

--- newDeployedESP32.py
+++ newDeployedESP32.py
@@ -126,6 +126,7 @@ def main():
             sendList[0 : 6] = [''.join(sendList[0 : 6])]
             sendList[7 : 11] = [''.join(sendList[7 : 11])]
             sendList[13 : 17] = [''.join(sendList[13 : 17])]
+        print(type(sendList))
         print(sendList)
 
 
