32dea520d575c01403209a6382f341735a75ff75
25nlambert <82062702+25nlambert@users.noreply.github.com>
2022-03-28 15:12:41 -0400

    Add files via upload

--- newDeployedESP32.py
+++ newDeployedESP32.py
@@ -124,12 +124,13 @@ def main():
 
         for x in tempList:
             sendList.extend(chr(x))
-            sendList[0:6] = [''.join(sendList[0 : 6])]
-            sendList[7:12] = [''.join(sendList[7 : 12])]
-            sendList[13:18] = [''.join(sendList[13 : 18])]
+            s
             #sendList[0 : 6] = [''.join(sendList[0 : 6])]
             #sendList[7 : 11] = [''.join(sendList[7 : 11])]
             #sendList[13 : 17] = [''.join(sendList[13 : 17])]
+        endList[0:6] = [''.join(sendList[0 : 6])]
+        sendList[7:12] = [''.join(sendList[7 : 12])]
+        sendList[13:18] = [''.join(sendList[13 : 18])]
         print(type(sendList))
         print(sendList)
 
